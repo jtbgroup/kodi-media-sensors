bb0bbf1cd5a0eb54380d2b043584a84ff40e3396
Gautier Vanderslyen <gautier.vanderslyen@gmail.com>
2021-03-02 14:53:19 +0100

    last modifications

--- custom_components/kodi_media_sensors/const.py
+++ custom_components/kodi_media_sensors/const.py
@@ -21,16 +21,18 @@ KEY_SONGS = "songs"
 KEY_ARTISTS = "artists"
 KEY_MOVIES = "movies"
 KEY_TVSHOWS = "tvshows"
+KEY_TVSHOW_SEASONS = "seasons"
+KEY_TVSHOW_SEASON_DETAILS = "seasondetails"
+KEY_TVSHOW_EPISODES = "episodes"
 KEY_ALBUM_DETAILS = "albumdetails"
 
 ENTITY_SENSOR_RECENTLY_ADDED_TVSHOW = "kodi_media_sensor_recently_added_tvshow"
 ENTITY_SENSOR_RECENTLY_ADDED_MOVIE = "kodi_media_sensor_recently_added_movie"
 ENTITY_SENSOR_PLAYLIST = "kodi_media_sensor_playlist"
 ENTITY_SENSOR_SEARCH = "kodi_media_sensor_search"
-
 ENTITY_NAME_SENSOR_RECENTLY_ADDED_TVSHOW = (
     "Kodi Media Sensor Recently Added TV Episodes"
 )
 ENTITY_NAME_SENSOR_RECENTLY_ADDED_MOVIE = "Kodi Media Sensor Recently Added Movies"
-ENTITY_NAME_SENSOR_PLAYLIST = "Kodi Media Sensor Palylist"
+ENTITY_NAME_SENSOR_PLAYLIST = "Kodi Media Sensor Playlist"
 ENTITY_NAME_SENSOR_SEARCH = "Kodi Media Sensor Search"
